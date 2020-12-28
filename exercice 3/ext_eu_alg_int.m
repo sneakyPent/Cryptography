7a55ef2244fe21f6adb4ac998b94ef4a4716c316
sneakyPent <zaharioudakis@yahoo.com>
2020-12-28 16:35:09 +0200

    extended euclidean algorithm done

---
+++ exercice 3/ext_eu_alg_int.m
@@ -0,0 +1,27 @@
+function [g, s, t] = ext_eu_alg_int(a, b)
+
+%   g = r*r_0 + r   
+
+    g = a;
+    s = 1;
+    t = 0;
+    r_0 = b;
+    x_0 = 0; 
+    y_0 = 1;
+    
+    
+    while r_0 ~= 0
+        r = mod(g,r_0);
+        q = fix(g / r_0);
+       
+        g = r_0;
+        r_0 = r;
+        x = s - q*x_0;
+        s = x_0;
+        x_0 = x;
+        y = t - q*y_0;
+        t = y_0;
+        y_0 = y;
+    end
+    return
+end
