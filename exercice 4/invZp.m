98cd7dff89361a78c25448a25d47bbcbad50e144
sneakyPent <zaharioudakis@yahoo.com>
2021-01-13 18:25:41 +0200

    fix bug for R[x] return 1/x

--- exercice 4/invZp.m
+++ exercice 4/invZp.m
@@ -6,7 +6,11 @@ function z = invZp(x, p)
 %     1) Use the euklidean algorithm to find the inverse of x, x^(-1) (mod p),
 %     2) calculate the z = x mod p.
 
-    addpath('../exercice 3')
-    [g,s,t] = ext_eu_alg_int(x,p);
+%     addpath('../exercice 3')
+    if p==0
+        z = 1/x;
+        return
+    end
+    [g,s,t] = ext_euc_alg_int(x,p);
     z = mod(s, p);
 end
