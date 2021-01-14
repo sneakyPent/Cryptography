cf6db319e59eee6f767956da7057d89cd6ce4087
sneakyPent <zaharioudakis@yahoo.com>
2021-01-14 15:24:57 +0200

    minor changes on exercice 6 implementation

--- exercice 6/exercice_6.m
+++ exercice 6/exercice_6.m
@@ -8,14 +8,12 @@
 %% A
 % Run cyc_encodeZ2 function for message m=[0 1 1 1]
 n = 15;
-k = 11;
 gen = [1 1 0 0 1];
 m=[0 1 1 1];
 [nonSystematic, systematic] = cyc_encodeZ2(n,gen,m)
 %% B
 % decode the codeword c = [1 1 0 1 1 1 0 0 0 1 1 1 0 1] 
 n = 15;
-k = 11;
 gen = [1 1 0 0 1];
 c=[1 1 0 1 1 1 0 0 0 1 1 1 0 1];
 c_padded =  [c, zeros(1, n - length(c))];
@@ -34,7 +32,6 @@ sysMesssage = c_padded(end-( k-1):end)
 
 %% C
 n = 15;
-k = 11;
 gen = [1 1 0 0 1];
 r=[0 0 0 1 0 1 0 0 0 0 1 0 0 0 1];
 [syndrom, error] = syndrom_comp_error_detect(n,gen,r)
